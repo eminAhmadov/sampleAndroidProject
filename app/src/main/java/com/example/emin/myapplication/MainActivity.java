08382817add770ccbbddfc6a0506e145836b7fe3
DESKTOP-VJADCT3\Emin <emin.ahmadov99@gmail.com>
2018-08-05 00:55:33 +0200

    added menu and "Clear" item.

--- app/src/main/java/com/example/emin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/emin/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -85,4 +87,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menu_clear){
+            number.setText("0");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
